d3ab61e996dd463b4ad1f93bfc6c8553c36d9b31
PayaLsRaut <payalraut107@gmail.com>
2022-05-29 21:31:57 +0530

    Gambling Program Started

--- Gambler.java
+++ Gambler.java
@@ -1,42 +1,11 @@
 package com.bridgelabz;
 import java.util.Random;
-    public class Gambler
-    {
-        public static int EveryDayStake = 100, EveryGame = 1;
-        public void bet()
-        {
-            EveryDayStake=EveryDayStake-1;
-        }
-        public void calculateresign()
-        {
-            double stakepercentage;
-            Random rand=new Random();
-            int i=1;
-            while (i<=20)
-            {
-                System.out.println("day:"+i);
-                int temp = rand.nextInt(2);
-                if(temp==1)
-                {
-                    EveryDayStake=EveryDayStake+1;
-                    System.out.print("won today by 1 ");
-                }
-                else
-                {
-                    EveryDayStake=EveryDayStake-1;
-                    System.out.print("lost today by 1  ");
-                }
-                System.out.println("total amount "+EveryDayStake);
-                i++;
-            }
-        }
-        public static void main(String[] args)
-        {
+    public class Gambler {
+        public static final int EveryDayStake = 100, EveryGame = 1;
 
-            Gambler person=new Gambler();
-            person.bet();
-            person.calculateresign();
+        public static void main(String[] args) {
+
+
         }
 
     }
-
